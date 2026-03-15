162651304717e5b89aebeb2dd02469d1bfb24564
Himanshu Shekhar <himanshekhar03@.com>
2026-03-15 11:18:44 +0530

    Fix: add Linux CI Chrome flags

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -3,16 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
-def get_driver(headless: bool = False) -> webdriver.Chrome:
+def get_driver(headless: bool = True) -> webdriver.Chrome:
     headless = os.getenv("HEADLESS", str(headless)).lower() == "true"
 
     options = ChromeOptions()
-    if headless:
-        options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-setuid-sandbox")
 
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(0)
